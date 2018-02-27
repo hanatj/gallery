6fd5a299e23a0a2d5f9d85e5850f035fcdff04bd
walidmash <waleedmash2@gmail.com>
2018-02-27 20:50:55 +0200

    remove spaces

--- src/views/reducers/index.js
+++ src/views/reducers/index.js
@@ -1,9 +1,6 @@
 import { combineReducers } from 'redux';
 
 export default combineReducers({
-<<<<<<< 16bd60f489d7c3816837e00158fc90fcf3abad30
 // TODO:
-=======
-// TODO: 
->>>>>>> main structure
+
 });
